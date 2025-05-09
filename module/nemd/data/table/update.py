2d30b31620750f6b8d090a0fa3df58ba29e62679
zhteg4 <zhteg4@gmail.com>
2025-05-08 22:38:58 -0400

    data_table_test.py added

--- module/nemd/data/table/update.py
+++ module/nemd/data/table/update.py
@@ -7,14 +7,36 @@ import mendeleev
 import pandas as pd
 from rdkit import Chem
 
-table = Chem.GetPeriodicTable()
-MAX_ATOMIC_NUM = table.GetMaxAtomicNumber()
-symbols = [table.GetElementSymbol(x) for x in range(MAX_ATOMIC_NUM + 1)]
-atomic_numbers = [x for x in range(MAX_ATOMIC_NUM + 1)]
-atomic_weights = [table.GetAtomicWeight(x) for x in range(MAX_ATOMIC_NUM + 1)]
-cpk_colors = [None] + [mendeleev.element(x).cpk_color for x in symbols[1:]]
-data = dict(atomic_number=atomic_numbers,
-            atomic_weight=atomic_weights,
-            cpk_color=cpk_colors)
-data = pd.DataFrame(data, index=pd.Index(symbols, name='symbol'))
-data.to_parquet('table.parquet')
+
+class Table:
+    """
+    Periodic Table.
+    """
+
+    def __init__(self):
+        self.data = None
+        self.setUp()
+
+    def setUp(self):
+        """
+        Set up.
+        """
+        table = Chem.GetPeriodicTable()
+        atomic_numbers = [x for x in range(table.GetMaxAtomicNumber() + 1)]
+        symbols = [table.GetElementSymbol(x) for x in atomic_numbers]
+        cpk_colors = [mendeleev.element(x).cpk_color for x in symbols[1:]]
+        atomic_weights = [table.GetAtomicWeight(x) for x in atomic_numbers]
+        data = dict(atomic_number=atomic_numbers,
+                    atomic_weight=atomic_weights,
+                    cpk_color=[None] + cpk_colors)
+        self.data = pd.DataFrame(data, index=pd.Index(symbols, name='symbol'))
+
+    def write(self):
+        """
+        Write data.
+        """
+        self.data.to_parquet('table.parquet')
+
+
+if __name__ == "__main__":
+    Table().write()
---
+++ test/unit/module/data_table_test.py
@@ -0,0 +1,18 @@
+import os.path
+
+import pytest
+
+from nemd.data.table import update
+
+
+@pytest.mark.slow
+class TestSmiles:
+
+    @pytest.fixture
+    def table(self):
+        return update.Table()
+
+    def testWrite(self, table, tmp_dir):
+        (119, 3) == table.data.shape
+        table.write()
+        assert os.path.exists('table.parquet')
