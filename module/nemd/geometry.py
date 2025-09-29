a2aeb0a498cccba46f36614047e0f04995859b45
tengzhang <tengzhang@gmail.com>
2025-09-28 20:21:07 -0400

    oop for module/nemd/geometry.py

--- module/nemd/geometry.py
+++ module/nemd/geometry.py
@@ -6,45 +6,54 @@ Geometry calculation of 3D points, such as distance, angle, and dihedral angle.
 import numpy as np
 
 
-def distance(pnts):
+class Point(np.ndarray):
     """
-    Calculate distance between two points.
-
-    :param pnts list of two ndarrays: The coordinates of two points.
-    :return float: The distance between two points.
+    Class to perform geometry calculations on points.
     """
-    return np.linalg.norm(pnts[0] - pnts[1])
 
+    def __new__(cls, array):
+        """
+        :param array np.ndarray: each row is a point.
+        """
+        return np.asarray(array).view(cls)
 
-def angle_vs(vecs):
-    """
-    Calculate angle between two vectors.
+    def distance(self):
+        """
+        Calculate distance between two points: The coordinates of two points.
 
-    :param vecs list of two ndarrays: The two vectors forms the angle.
-    :return float: The angle between two vectors in degrees.
-    """
-    product = np.dot(vecs[0], vecs[1])
-    cos = product / np.linalg.norm(vecs[0]) / np.linalg.norm(vecs[1])
-    return np.arccos(cos) / np.pi * 180.
+        :return float: The distance between two points.
+        """
+        return np.linalg.norm(self[0] - self[1])
 
+    def angle(self):
+        """
+        Calculate angle formed by three points: point 0-1-2 forms the angle.
 
-def angle(pnts):
-    """
-    Calculate angle formed by three points.
+        :return float: The angle formed by the three points in degrees.
+        """
+        end_points = self[::2, :]
+        return self.angle_vs(end_points - self[1])
 
-    :param pnts list of three ndarrays: point 0-1-2 forms the angle.
-    :return float: The angle formed by the three points in degrees.
-    """
-    end_points = pnts[::2, :]
-    return angle_vs(end_points - pnts[1])
+    @staticmethod
+    def angle_vs(vecs):
+        """
+        Calculate angle between two vectors.
 
+        :param vecs list of two ndarrays: The two vectors forms the angle.
+        :return float: The angle between two vectors in degrees.
+        """
+        product = np.dot(vecs[0], vecs[1])
+        cos = product / np.linalg.norm(vecs[0]) / np.linalg.norm(vecs[1])
+        return np.arccos(cos) / np.pi * 180.
 
-def dihedral(pnts):
-    """
-    Calculate dihedral angle formed by four points.
+    def dihedral(self):
+        """
+        Calculate dihedral angle formed by four points: points 0-1-2-3 forms the
+        dihedral angle.
 
-    :param pnts list of four ndarrays: points 0-1-2-3 forms the dihedral angle.
-    :return float: The dihedral angle formed by the four points in degrees.
-    """
-    bonds = pnts[:3, :] - pnts[1:, :]
-    return angle_vs([np.cross(*bonds[:2, :]), np.cross(*bonds[1:, :])])
+        :return float: The dihedral angle formed by the four points in degrees.
+        """
+        bonds = self[:3, :] - self[1:, :]
+        return self.angle_vs(
+            [np.cross(*bonds[:2, :]),
+             np.cross(*bonds[1:, :])])
